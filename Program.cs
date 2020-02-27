7fe9255948e0bd7b68acf30789ea971e6ae1fa70
iancarpenter <ianhcarpenter@gmail.com>
2020-02-27 21:06:42 +0000

    Completed Version

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var repositories = await ProcessRepositories();
+
+            foreach(var repo in repositories)
+            {
+                System.Console.WriteLine(repo.Name);
+                System.Console.WriteLine(repo.Description);
+                System.Console.WriteLine(repo.GitHubHomeUrl);
+                System.Console.WriteLine(repo.Homepage);
+                System.Console.WriteLine(repo.Watchers);
+                System.Console.WriteLine(repo.lastPush);
+                System.Console.WriteLine();
+            }
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            // var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var respositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            return respositories;            
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.Json.Serialization;
+using System.Globalization;
+
+namespace WebApiClient
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public string JsonDate { get; set; }
+
+        public DateTime lastPush => 
+           DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+    }
+}
